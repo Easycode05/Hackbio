21818fc04412bbd3ceace3dbd8cf3173f77acb21
Ojo Israel <108278778+Easycode05@users.noreply.github.com>
2025-12-01 17:58:26 +0100

    Update comments for clarity in R_script.R

--- Stage 3/Scripts/R_script.R
+++ Stage 3/Scripts/R_script.R
@@ -12,23 +12,23 @@ counts <- read.delim("counts2.txt", header = TRUE, sep = "\t", stringsAsFactors 
 meta <- read.csv("metadata.csv", header = TRUE, stringsAsFactors = TRUE)
 meta$condition <- factor(meta$Phase, levels = c("Chronic Infection", "Acute Infection"))
 
-# === 2. Prepare metadata ===
+# 2. Prepare metadata 
 # Make sure the metadata has a column named 'sample'
 # and set it as rownames (so DESeq2 can match samples)
 rownames(meta) <- meta$Sample
 
-# === 3. Keep only count columns ===
+# 3. Keep only count columns 
 # Drop annotation columns like Chr, Start, End, Strand, Length
 counts_mat <- counts[, !(colnames(counts) %in% c("Chr", "Start", "End", "Strand", "Length"))]
 
-# === 4. Add Geneid as rownames ===
+# 4. Add Geneid as rownames 
 rownames(counts_mat) <- counts_mat$Geneid
 counts_mat <- counts_mat[, -1]  # remove Geneid column
 
-# === 5. Match order of samples ===
+# 5. Match order of samples
 counts_mat <- counts_mat[, rownames(meta)]
 
-# === 6. Sanity check ===
+# 6. Sanity check 
 cat("\n✅ Samples in counts:", length(colnames(counts_mat)))
 cat("\n✅ Samples in metadata:", length(rownames(meta)), "\n")
 
@@ -36,7 +36,7 @@ if (!all(colnames(counts_mat) == rownames(meta))) {
   stop("❌ Sample names in counts and metadata do not match. Please check!")
 }
 
-# === 7. Preview ===
+# 7. Preview 
 cat("\nPreview (samples only):\n")
 print(head(counts_mat[, 1:5]))
 
@@ -44,7 +44,7 @@ cat("\nPreview (with Geneid as rownames):\n")
 print(head(counts_mat))
 
 
-# === 8. Create DESeq2 dataset ===
+# 8. Create DESeq2 dataset 
 dds <- DESeqDataSetFromMatrix(countData = counts_mat,
                               colData = meta,
                               design = ~ Phase) 
@@ -60,7 +60,7 @@ dds$Phase
 #Perform the differential expression analysis
 dds <- DESeq(dds)
 
-# --- 4. Results
+# 4. Results
 res <- results(dds, contrast = c("Phase", "Acute Infection", "Chronic Infection"))
 res <- res[order(res$padj), ]
 
@@ -106,7 +106,7 @@ ggplot(volcano_data, aes(x = log2FoldChange, y = -log10(padj), color = threshold
   ggtitle("Volcano Plot: Acute vs Chronic PJI")
 dev.off()
 
-# --- 8. Heatmap of top 30 DEGs
+# 8. Heatmap of top 30 DEGs
 topGenes <- head(order(res$padj), 30)
 png("Heatmap_sa_top30.png", width = 7, height = 8, units = "in", res = 300)
 pheatmap(assay(rld)[topGenes, , drop = FALSE],
